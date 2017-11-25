1c9834f6179deeb36c0468e4809e76715c4c61b3
邓永庆 <dengyongqing@zhongan.io>
2017-11-25 22:40:44 +0800

    every hour

--- one_strategy/db/init.py
+++ one_strategy/db/init.py
@@ -82,10 +82,10 @@ def job():
     except Exception as e:
         print(e)
 
-schedule.every(10).seconds.do(job)
+# schedule.every(60).seconds.do(job)
 # schedule.every(10).minutes.do(job)
-# schedule.every().hour.do(job)
-# schedule.every().day.at("10:30").do(job)
+schedule.every().hour.do(job)
+# schedule.every().day.at("17:00").do(job)
 # schedule.every().monday.do(job)
 # schedule.every().wednesday.at("13:15").do(job)
 
