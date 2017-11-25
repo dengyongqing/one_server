1e0b3f63679521de7ad2422d8e755e44e9f139d5
邓永庆 <dengyongqing@zhongan.io>
2017-11-25 21:21:54 +0800

    add industry_classified

--- one_strategy/db/init.py
+++ one_strategy/db/init.py
@@ -1,4 +1,3 @@
--*-coding:utf-8 -*-
 from sqlalchemy import create_engine
 from django.http import HttpResponse
 import json
@@ -9,23 +8,24 @@ import time,sched
 import schedule
 import time
 
+def init(request):
+    # ts.get_stock_basics()
+    schedule.clear()
+    return HttpResponse('ok')
+
 def job():
     print("I'm working...")
-    # engine = create_engine('postgresql://postgres@localhost:5432/tushare') 
-    engine = create_engine('postgresql://postgres@47.93.193.128:5432/tushare')
-    temp = ts.get_stock_basics()
-    data = pd.DataFrame(temp)
-    
+    engine = create_engine('postgresql://postgres@localhost:5432/tushare') 
+    # engine = create_engine('postgresql://tushare@localhost:5432/tushare') 
+    # engine = create_engine('postgresql://postgres@47.93.193.128:5432/tushare')
     try:
-        # 股票列表
         stock_basics = ts.get_stock_basics()
         data = pd.DataFrame(stock_basics)
         data.to_sql('stock_basics',engine,index=False,if_exists='replace')
 
-        # 行业分类
         industry_classified = ts.get_industry_classified()
-        data = pd.DataFrame(industry_classified)
-        data.to_sql('industry_classified',engine,index=False,if_exists='replace')
+        data1 = pd.DataFrame(industry_classified)
+        data1.to_sql('industry_classified',engine,index=False,if_exists='replace')
     except Exception as e:
         print(e)
 
@@ -36,10 +36,10 @@ schedule.every(10).seconds.do(job)
 # schedule.every().monday.do(job)
 # schedule.every().wednesday.at("13:15").do(job)
 
+
 while True:
     schedule.run_pending()
     time.sleep(1)
-
 
 
 # def worker(msg, starttime):
