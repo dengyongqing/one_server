cfc8b3578c6d9c67e74450566796dac7c0bfdcf9
邓永庆 <dengyongqing@zhongan.io>
2017-12-14 18:37:06 +0800

    hide db init

--- one_strategy/codes.py
+++ one_strategy/codes.py
@@ -6,14 +6,19 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 import time,sched
-
+import pandas as pd
 
 def hello(request):
     # ts.get_stock_basics()
-    while True:  
-      print 'time: ', time.time()  
-      time.sleep(1) 
-    return HttpResponse(json.dumps(ts.get_stock_basics()))
+    stock_basics = ts.get_stock_basics()
+    data = pd.DataFrame(stock_basics)
+    data = data[(data['npr']>30) & (data['gpr']>30) & (data['rev']>20) & (data['profit']>20)]
+    # data.filter("npr>30 and gpr>30 and rev>20 and profit>100")
+    # data = data.filter("gpr>30")
+    # data = data.filter("rev>20")
+    # data = data.filter("profit>50")
+
+    return HttpResponse(json.dumps(data))
 
 def worker(msg, starttime):
     print "time:", time.time(), "msg", msg, 'startTime', starttime
